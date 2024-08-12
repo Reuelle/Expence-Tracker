65154ea388525578f05c90171ba6417e4533d61a
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-12 13:44:01 +0800

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
-
 // src/components/Header/Header.js
 import React from 'react';
-import Logo from '../Logo/Icon.svg';
+import Logo from '../Logo/Logo';
 import './Header.css'; // Optional: For custom styling
 
 const Header = () => (
   <header className="header">
-    <Logo /> {/* Include the Logo component */}
+    <Logo /> {/* Logo with connected "EXPENSE TRACKER" text */}
     <nav className="nav">
       {/* Add navigation links or other elements as needed */}
     </nav>
