f65a9ed1db003cddf71e2f3eb626b89f825848d9
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-12 13:42:31 +0800

    Update Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,13 @@
 // src/components/Logo/Logo.js
 import React from 'react';
-import logo from './Icon.png'; // Import your logo image
+import logo from './logo.svg'; // Import your logo image
 import './Logo.css'; // Optional: For custom styling
 
 const Logo = () => (
   <div className="logo-container">
     <img src={logo} alt="Expense Tracker Logo" className="logo" />
+    <span className="logo-text">EXPENSE TRACKER</span>
   </div>
 );
 
 export default Logo;
-
