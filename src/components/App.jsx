e10a26278c74384304cfcad50600deb66f1690c4
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-12 13:38:33 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
 import HomePage from './components/HomePage/HomePage';
 import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
@@ -8,6 +9,7 @@ import MainTransactionPage from './components/MainTransactionPage/MainTransactio
 
 const App = () => (
   <Router>
+    <Header /> {/* Include the Header component */}
     <Switch>
       <Route path="/" exact component={HomePage} />
       <Route path="/authorization" component={AuthorizationPage} />
