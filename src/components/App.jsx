e9a0b61ec73f58281af285c777f92131358d4fcd
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-12 13:25:16 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
+import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
+import MainTransactionPage from './components/MainTransactionPage/MainTransactionPage';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/authorization" component={AuthorizationPage} />
+      <Route path="/transaction-history" component={TransactionHistoryPage} />
+      <Route path="/main-transaction" component={MainTransactionPage} />
+    </Switch>
+  </Router>
+);
+
+export default App;
