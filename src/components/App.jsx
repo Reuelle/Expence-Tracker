2c7d3e135e25057601f49819f57d3e9f5a3f1371
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-14 09:31:29 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomePage from './components/Homepage/Homepage';
+import HomePage from './components/HomePage/HomePage';
+import RegisterPage from './components/RegisterPage/RegisterPage'; // Import RegisterPage component
 import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
 import MainTransactionPage from './components/MainTransactionPage/MainTransactionPage';
@@ -10,6 +11,7 @@ const App = () => (
   <Router>
     <Switch>
       <Route path="/" exact component={HomePage} />
+      <Route path="/register" component={RegisterPage} /> {/* Add route for RegisterPage */}
       <Route path="/authorization" component={AuthorizationPage} />
       <Route path="/transaction-history" component={TransactionHistoryPage} />
       <Route path="/main-transaction" component={MainTransactionPage} />
