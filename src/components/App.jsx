280baf915cd82d9d394d8dad20020720e4dbf3bc
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-14 14:24:49 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from '../components/HomePage/HomePage';
-import RegisterPage from './components/RegisterPage/RegisterPage'; // Import RegisterPage component
-import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
-import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
-import MainTransactionPage from './components/MainTransactionPage/MainTransactionPage';
+import RegisterPage from '../components/RegisterPage/RegisterPage'; // Import RegisterPage component
+import AuthorizationPage from '../components/AuthorizationPage/AuthorizationPage';
+import TransactionHistoryPage from '../components/TransactionHistoryPage/TransactionHistoryPage';
+import MainTransactionPage from '../components/MainTransactionPage/MainTransactionPage';
 
 const App = () => (
   <Router>
