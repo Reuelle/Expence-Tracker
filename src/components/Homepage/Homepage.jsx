05a9791cd2f35fb5f3940e10d721c318abc0abd7
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-14 13:47:12 +0800

    Update Homepage.jsx

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,20 +1,25 @@
 // src/components/HomePage/HomePage.jsx
 import React from 'react';
 import AuthNav from '../AuthNav/AuthNav';
-import AllUsersTab from '../AllUsersTab/AllUsersTab'; // Import AllUsersTab component
-import styles from './HomePage.module.css'; // Optional: For custom styling
+import AllUsersTab from '../AllUsersTab/AllUsersTab'; 
+import Header from '../Header/Header'; // Import the Header component
+import styles from './HomePage.module.css'; // Import CSS module for styling
 
 const HomePage = () => {
   return (
     <div className={styles.homePage}>
-      <header className={styles.header}>
-         <h3>EXPENSE LOG</h3>
+      <Header /> {/* Render the Header component */}
+      <div className={styles.mainContent}>
         <h1>Manage Your Finances Masterfully!</h1>
-        <p>ExpenseTracker effortlessly empowers you to take controlof your finaces!with intuitive features,it simplifies the process of tracking and managing expenses,allowing for a stress-free mastery over your financial world. </p>
-      </header>
-      <AuthNav />
-      <div className={styles.content}>
-        <AllUsersTab /> {/* This will only be visible on desktop */}
+        <p>
+          ExpenseTracker effortlessly empowers you to take control of your finances! 
+          With intuitive features, it simplifies the process of tracking and managing expenses, 
+          allowing for a stress-free mastery over your financial world.
+        </p>
+        <AuthNav /> {/* Navigation for Register/Login */}
+        <div className={styles.content}>
+          <AllUsersTab /> {/* This will only be visible on desktop */}
+        </div>
       </div>
     </div>
   );
