59ba2e6780ca0b93b4dad7af977e03e5c399c7ef
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-15 09:03:15 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage/HomePage'; // Correct import path
-import RegisterPage from './components/RegisterPage/RegisterPage'; // Correct import path
+import HomePage from './components/HomePage/HomePage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
 import MainTransactionPage from './components/MainTransactionPage/MainTransactionPage';
