249b46a89c60c5524adfb6b13ae23cb31ecd2fe6
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-15 08:39:44 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-// src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from '../components/HomePage/HomePage';
-import RegisterPage from '../components/RegisterPage/RegisterPage'; // Import RegisterPage component
+import RegisterPage from '../components/RegisterPage/RegisterPage';
 import AuthorizationPage from '../components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from '../components/TransactionHistoryPage/TransactionHistoryPage';
 import MainTransactionPage from '../components/MainTransactionPage/MainTransactionPage';
@@ -10,11 +9,11 @@ import MainTransactionPage from '../components/MainTransactionPage/MainTransacti
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/register" component={RegisterPage} /> {/* Add route for RegisterPage */}
-      <Route path="/authorization" component={AuthorizationPage} />
-      <Route path="/transaction-history" component={TransactionHistoryPage} />
-      <Route path="/main-transaction" component={MainTransactionPage} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/authorization" element={<AuthorizationPage />} />
+      <Route path="/transaction-history" element={<TransactionHistoryPage />} />
+      <Route path="/main-transaction" element={<MainTransactionPage />} />
     </Routes>
   </Router>
 );
