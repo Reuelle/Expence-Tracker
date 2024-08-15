a1c2214d3f5ab55bfb8e19c207c591b76d88a5bf
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-15 09:02:54 +0800

    Update UserSetsModal.jsx

--- src/components/UserSetsModal/UserSetsModal.jsx
+++ src/components/UserSetsModal/UserSetsModal.jsx
@@ -1,10 +1,11 @@
 // src/components/UserSetsModal/UserSetsModal.jsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { updateUser, uploadAvatar, removeAvatar } from 'redux/User/User-operations'; // Adjust path as needed
 import { showErrorNotification, showSuccessNotification } from 'utils/notifications'; // Adjust path as needed
 import styles from './UserSetsModal.module.css'; // Import CSS module
+
 
 const UserSetsModal = ({ onClose }) => {
   const [formData, setFormData] = useState({
@@ -18,7 +19,6 @@ const UserSetsModal = ({ onClose }) => {
 
   useEffect(() => {
     // Fetch user data from the backend or global state and set it to formData
-    // Replace with actual data fetching logic
     const fetchUserData = async () => {
       try {
         // Fetch user data
@@ -72,18 +72,18 @@ const UserSetsModal = ({ onClose }) => {
     }
   };
 
-  const handleEscapeKey = (e) => {
+  const handleEscapeKey = useCallback((e) => {
     if (e.key === 'Escape') {
       onClose();
     }
-  };
+  }, [onClose]);
 
   useEffect(() => {
     window.addEventListener('keydown', handleEscapeKey);
     return () => {
       window.removeEventListener('keydown', handleEscapeKey);
     };
-  }, []);
+  }, [handleEscapeKey]);
 
   return (
     <div className={styles.modalOverlay} onClick={handleBackdropClick}>
@@ -107,7 +107,6 @@ const UserSetsModal = ({ onClose }) => {
           <label>
             Currency:
             <select name="currency" value={formData.currency} onChange={handleInputChange}>
-              {/* Populate with currency options */}
               <option value="USD">USD</option>
               <option value="EUR">EUR</option>
               {/* Add other currencies */}
