60e732dcbd1114c1088726e8cd3735e301588a06
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-15 08:43:48 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../components/HomePage/HomePage';
+import RegisterPage from '../components/RegisterPage/RegisterPage';
+import AuthorizationPage from '../components/AuthorizationPage/AuthorizationPage';
+import TransactionHistoryPage from '../components/TransactionHistoryPage/TransactionHistoryPage';
+import MainTransactionPage from '../components/MainTransactionPage/MainTransactionPage';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/authorization" element={<AuthorizationPage />} />
+      <Route path="/transaction-history" element={<TransactionHistoryPage />} />
+      <Route path="/main-transaction" element={<MainTransactionPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
