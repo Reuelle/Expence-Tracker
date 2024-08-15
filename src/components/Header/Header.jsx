319ff2a90c7a5d44b4453e9d08b38017caf4cbe2
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-15 14:53:12 +0800

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,6 @@ import './Header.module.css'; // Import custom styles
 const Header = () => (
   <header className="header">
     <div className="logo">
-      EXPENSE TRACKER
     </div>
     <nav className="nav">
       {/* Add navigation links or other elements as needed */}
