e6ac0510eab297787ff5d490e71e0d8e03cf2882
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-08-22 13:22:30 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'; // Import Header
 import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import AuthorizationPage from './components/AuthorizationPage/AuthorizationPage';
 import TransactionHistoryPage from './components/TransactionHistoryPage/TransactionHistoryPage';
 import MainTransactionPage from './components/MainTransactionPage/MainTransactionPage';
 
 const App = () => (
-  <BrowserRouter>
-    <Header /> {/* Add Header here if it should be on all pages */}
+  <Router basename="/expense-tracker">
+    <Header /> {/* Ensure Header is only rendered here */}
     <Routes>
       <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/authorization" element={<AuthorizationPage />} />
       <Route path="/transaction-history" element={<TransactionHistoryPage />} />
       <Route path="/main-transaction" element={<MainTransactionPage />} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
